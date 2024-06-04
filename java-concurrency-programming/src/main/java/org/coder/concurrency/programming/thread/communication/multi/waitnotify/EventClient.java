ca911b9314cc4b1186cab72f7b54e1cce3fa2837
夜骐 <crayzer.chen@gmail.com>
2024-06-05 01:18:49 +0800

    refactor(Thread):

--- java-concurrency-programming/src/main/java/org/coder/concurrency/programming/thread/communication/multi/waitnotify/EventClient.java
+++ java-concurrency-programming/src/main/java/org/coder/concurrency/programming/thread/communication/multi/waitnotify/EventClient.java
@@ -1,6 +1,7 @@
 package org.coder.concurrency.programming.thread.communication.multi.waitnotify;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 /**
  * 多个 Producer 线程和多个 Consumer 线程，多线程间的通信
@@ -13,27 +14,43 @@ public class EventClient {
     public static void main(String[] args) {
         final EventQueue eventQueue = new EventQueue();
 
-        for (int i = 0; i < 2; i++) {
-            new Thread(() -> {
-                for (; ; ) {
-                    eventQueue.offer(new EventQueue.Event());
-                }
-            }, "Produce-" + i).start();
-        }
+//        for (int i = 0; i < 2; i++) {
+//            new Thread(() -> {
+//                for (; ; ) {
+//                    eventQueue.offer(new EventQueue.Event());
+//                }
+//            }, "Produce-" + i).start();
+//        }
+        // 等价于上面的写法
+        IntStream.range(1, 3).mapToObj(i -> new Thread(() -> {
+            for (; ; ) {
+                eventQueue.offer(new EventQueue.Event());
+            }
+        }, "Produce-" + i)).forEach(Thread::start);
 
-        for (int i = 0; i < 3; i++) {
-            new Thread(() -> {
-                for (; ; ) {
-                    eventQueue.take();
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+//        for (int i = 0; i < 3; i++) {
+//            new Thread(() -> {
+//                for (; ; ) {
+//                    eventQueue.take();
+//                    try {
+//                        TimeUnit.MILLISECONDS.sleep(10);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }, "Consumer-" + i).start();
+//        }
+        // 等价于上面的写法
+        IntStream.range(1, 4).mapToObj(i -> new Thread(() -> {
+            for (; ; ) {
+                eventQueue.take();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }, "Consumer-" + i).start();
-        }
-
+            }
+        }, "Consumer-" + i)).forEach(Thread::start);
 
 
     }
