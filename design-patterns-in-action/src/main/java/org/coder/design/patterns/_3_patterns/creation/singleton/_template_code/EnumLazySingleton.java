87ef884f252200e9f00692ca3d2d7620344e16e9
夜骐 <crayzer.chen@gmail.com>
2024-06-08 00:22:33 +0800

    refactor(design-patterns-in-action):

--- design-patterns-in-action/src/main/java/org/coder/design/patterns/_3_patterns/creation/singleton/_template_code/EnumLazySingleton.java
+++ design-patterns-in-action/src/main/java/org/coder/design/patterns/_3_patterns/creation/singleton/_template_code/EnumLazySingleton.java
@@ -39,7 +39,7 @@ public final class EnumLazySingleton {
             return instance;
         }
 
-        public static EnumLazySingleton getInstance() {
+        private static EnumLazySingleton getInstance() {
             return INSTANCE.getSingleton();
         }
     }
