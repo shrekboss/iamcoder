8f03732f6a56d740252207db21d7e54e35f235aa
夜骐 <crayzer.chen@gmail.com>
2024-06-07 19:21:24 +0800

    feat(concurrency-in-action): 常见观察者模式实现

---
+++ design-patterns-in-action/src/main/java/org/coder/design/patterns/_3_patterns/behavior/_1_observer/_template_code/Observer.java
@@ -0,0 +1,12 @@
+package org.coder.design.patterns._3_patterns.behavior._1_observer._template_code;
+
+/**
+ * 观察者的接口定义
+ *
+ * @author <a href="mailto:crayzer.chen@gmail.com">夜骐</a>
+ * @since 1.0.0
+ */
+public interface Observer {
+
+    void update(Message message);
+}
