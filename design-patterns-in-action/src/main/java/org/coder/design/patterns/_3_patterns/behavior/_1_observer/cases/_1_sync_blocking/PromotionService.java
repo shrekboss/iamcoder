6b824915ea8d0469f4c534aa8aca19f70b40ff84
夜骐 <crayzer.chen@gmail.com>
2024-06-07 19:27:09 +0800

    feat(concurrency-in-action): 观察者模式的同步阻塞的实现方式

---
+++ design-patterns-in-action/src/main/java/org/coder/design/patterns/_3_patterns/behavior/_1_observer/cases/_1_sync_blocking/PromotionService.java
@@ -0,0 +1,15 @@
+package org.coder.design.patterns._3_patterns.behavior._1_observer.cases._1_sync_blocking;
+
+/**
+ * @author <a href="mailto:crayzer.chen@gmail.com">夜骐</a>
+ * @since 1.0.0
+ */
+public class PromotionService {
+
+    /**
+     * 给用户发放投资体验金
+     */
+    public void issueNewUserExperienceCash(Long userId) {
+
+    }
+}
