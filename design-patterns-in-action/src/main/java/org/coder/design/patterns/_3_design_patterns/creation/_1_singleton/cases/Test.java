35858dc203ddbce37ec9edcd6669be7380e138f5
夜骐 <crayzer.chen@gmail.com>
2024-06-08 16:48:18 +0800

    style(design-patterns-in-action):

---
+++ design-patterns-in-action/src/main/java/org/coder/design/patterns/_3_design_patterns/creation/_1_singleton/cases/Test.java
@@ -0,0 +1,8 @@
+package org.coder.design.patterns._3_design_patterns.creation._1_singleton.cases;
+
+/**
+ * @author <a href="mailto:crayzer.chen@gmail.com">夜骐</a>
+ * @since 1.0.0
+ */
+public class Test {
+}
