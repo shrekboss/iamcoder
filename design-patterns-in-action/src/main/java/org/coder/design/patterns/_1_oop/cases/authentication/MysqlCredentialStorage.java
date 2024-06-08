2cacf0c48b93419928b7b9da3b867c05a729fb05
夜骐 <crayzer.chen@gmail.com>
2024-06-09 00:03:06 +0800

    feat(design-patterns-in-action): 开发接口鉴权功能

---
+++ design-patterns-in-action/src/main/java/org/coder/design/patterns/_1_oop/cases/authentication/MysqlCredentialStorage.java
@@ -0,0 +1,12 @@
+package org.coder.design.patterns._1_oop.cases.authentication;
+
+/**
+ * @author <a href="mailto:crayzer.chen@gmail.com">夜骐</a>
+ * @since 1.0.0
+ */
+public class MysqlCredentialStorage implements CredentialStorage {
+    @Override
+    public String getPasswordByAppId(String appId) {
+        return null;
+    }
+}
