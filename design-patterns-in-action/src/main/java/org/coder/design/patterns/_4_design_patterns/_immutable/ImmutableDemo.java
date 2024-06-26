ad7d3bf14b9c6e5408d407c51f3b9f4eb9430756
夜骐 <crayzer.chen@gmail.com>
2024-06-26 22:49:20 +0800

    feat(design-patterns-in-action): Immutable 模式(非 GoF设计模式)

---
+++ design-patterns-in-action/src/main/java/org/coder/design/patterns/_4_design_patterns/_immutable/ImmutableDemo.java
@@ -0,0 +1,46 @@
+package org.coder.design.patterns._4_design_patterns._immutable;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * (what)
+ * <p>
+ * (why)
+ * <p>
+ * (how)
+ *
+ * @author <a href="mailto:crayzer.chen@gmail.com">夜骐</a>
+ * @since 1.0.0
+ */
+public class ImmutableDemo {
+    public static void main(String[] args) {
+        List<String> originalList = new ArrayList<>();
+        originalList.add("a");
+        originalList.add("b");
+        originalList.add("c");
+
+        List<String> jdkUnmodifiableList = Collections.unmodifiableList(originalList);
+        List<String> guavaImmutableList = ImmutableList.copyOf(originalList);
+
+        // 抛出UnsupportedOperationException
+        // jdkUnmodifiableList.add("d");
+        // 抛出UnsupportedOperationException
+        // guavaImmutableList.add("d");
+        originalList.add("d");
+
+        print(originalList); // a b c d
+        print(jdkUnmodifiableList); // a b c d
+        print(guavaImmutableList); // a b c
+    }
+
+    private static void print(List<String> list) {
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ design-patterns-in-action/src/main/java/org/coder/design/patterns/_4_design_patterns/_immutable/_readme.md
@@ -0,0 +1,8 @@
+## Google Guava 的不变集合类
+
+> Google Guava 针对集合类（Collection、List、Set、Map…）提供了对应的不变集合类（ImmutableCollection、ImmutableList、ImmutableSet、ImmutableMap…）和
+> Java JDK 提供了不变集合类（UnmodifiableCollection、UnmodifiableList、UnmodifiableSet、UnmodifiableMap…）有什么区别？
+
+参考代码：
+
+- [ImmutableDemo.java](ImmutableDemo.java)
