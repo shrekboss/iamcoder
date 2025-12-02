a034d854eafbdf99811eeb0057d02bd8dfe61336
tykj_chenyizhe <chenyizhe01@tansun.com.cn>
2025-12-02 15:08:58 +0800

    refactor(err-in-action):

--- algorithm-in-action/src/main/java/org/coder/algorithm/programming/package-info.java
+++ algorithm-in-action/src/main/java/org/coder/algorithm/programming/package-info.java
@@ -5,7 +5,7 @@
  * <p>
  * (how)
  *
- * @author <a href="mailto:chenyizhe01@tansun.com.cn">tykj_chenyizhe</a>
+ * @author <a href="mailto:crayzer.chen@gmail.com">chenyizhe</a>
  * @since 1.0.0
  */
 package org.coder.algorithm.programming;
